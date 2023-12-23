a74b5d103b82439a1fa3ce247e05a7a5b3c50e00
Mustafa <mmermmus123@gmail.com>
2023-12-23 21:44:21 +0300

    welcome page db changes, desk

--- app/app.py
+++ app/app.py
@@ -2,46 +2,42 @@ from flask import Flask, render_template, request, g
 import sqlite3
 
 app = Flask(__name__)
-app.config['DATABASE'] = './db/main.db'
+app.config["DATABASE"] = "./db/main.db"
+
 
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is None:
-        db = g._database = sqlite3.connect(app.config['DATABASE'])
+        db = g._database = sqlite3.connect(app.config["DATABASE"])
     return db
+
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is not None:
         db.close()
 
-#TODO welcome page.
-@app.route('/')
+
+# TODO welcome page.
+@app.route("/")
 def index():
-    cur = get_db().cursor()
-    cur.execute('SELECT * FROM role')
-    data = cur.fetchall()
-   # return render_template('index.html', data=data)
-    return render_template('user.html')
+    return render_template("welcome.html")
 
 
-@app.route('/desk/<string:desk_rfid>')
+@app.route("/desk/<string:desk_rfid>")
 def desk_access(desk_rfid):
-	connection = get_db()
-	cursor = connection.cursor()
+    connection = get_db()
+    cursor = connection.cursor()
 
-	cursor.execute('select * from desk where desk_rfid = ?',(desk_rfid,))
-	data = cursor.fetchone()
-	if data:
-		return f'Desk ID: {data[0]},  desk code: {data[1]}' # create a user page that takes data as parameter.
-	else:
-		return 'Invalid Desk' # create and return invalid desk html error page.
+    cursor.execute("select * from desk where desk_rfid = ?", (desk_rfid,))
+    data = cursor.fetchone()
 
-
-
+    if data:
+        return f"Desk ID: {data[0]},  desk code: {data[1]}"  # create a user page that takes data as parameter.
+    else:
+        return "Invalid Desk"  # create and return invalid desk html error page.
 
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- app/models/desk.py
+++ app/models/desk.py
@@ -1,11 +1,29 @@
 class Desk:
     def __init__(self, desk_id, desk_rfid, desk_role_id, status, rgb):
-        self.id = desk_id #1-2...
-        self.desk_rfid = desk_rfid #BA:65:D3:A1
-        self.desk_role_id = desk_role_id # 1 admin 2 user
-        self.status = status # 0 or 1
-        self.rgb = rgb # '255 255 200'
+        self.id = desk_id  # 1-2...
+        self.desk_rfid = desk_rfid  # BA:65:D3:A1
+        self.desk_role_id = desk_role_id  # 1 admin 2 user
+        self.status = status  # 0 or 1
+        self.rgb = rgb  # '0 255 255 200' led status, red, green, blue
 
     def __str__(self):
-        return f'Desk ID: {self.id}, RFID: {self.desk_rfid}, Role ID: {self.desk_role_id}, Status: {self.status}, RGB: {self.rgb}'
+        return f"Desk ID: {self.id}, RFID: {self.desk_rfid}, Role ID: {self.desk_role_id}, Status: {self.status}, RGB: {self.rgb}"
 
+    @classmethod
+    def parse(cls, desk_dict):
+        return cls(
+            desk_dict["id"],
+            desk_dict["desk_rfid"],
+            desk_dict["desk_role_id"],
+            desk_dict["status"],
+            desk_dict["rgb"],
+        )
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "desk_rfid": self.desk_rfid,
+            "desk_role_id": self.desk_role_id,
+            "status": self.status,
+            "rgb": self.rgb,
+        }
